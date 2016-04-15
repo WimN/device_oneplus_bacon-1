9138b25ee4f65d020fb5f502be3dab3a959ef1da
Martinusbe <martinusbe@gmail.com>
2016-04-15 10:23:59 +0200

    bc: disable some opts
    Change-Id: Ief9485dcbf13f400d6c250faf816810c86192aa9
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,16 +47,14 @@ BLOCK_BASED_OTA=false
 CLANG_O3 := true
 STRICT_ALIASING := false
 KRAIT_TUNINGS := true
-GRAPHITE_OPTS := true
+GRAPHITE_OPTS := false
 ENABLE_GCCONLY := true
-ENABLE_SANITIZE := true
+ENABLE_SANITIZE := false
 # Kernel Toolchain
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
 TARGET_GCC_VERSION_EXP := 4.9-uber
-
-USE_CLANG_PLATFORM_BUILD := false
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
