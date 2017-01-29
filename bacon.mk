4cd47f8713bb180df062723a12129f05325fd5a2
Sultanxda <sultanxda@gmail.com>
2017-01-28 16:19:46 -0800

    bacon: Use OSS camera HAL

--- bacon.mk
+++ bacon.mk
@@ -75,9 +75,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
-    camera.bacon \
-    libshim_camera \
-    libshim_cameraclient
+    camera.msm8974
 
 # Charger
 PRODUCT_PACKAGES += \
