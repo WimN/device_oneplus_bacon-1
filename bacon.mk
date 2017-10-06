b59d6bc5a86e93620b85b996de6b6abc1360c448
Kshitij Gupta <kshitijgm@gmail.com>
2017-10-07 02:44:43 +0530

    bacon: Cleanup camera, prepare for the OSS hal

--- bacon.mk
+++ bacon.mk
@@ -75,7 +75,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
-    libshim_camera \
     camera.msm8974
 
 # Charger
